5a7585f9722444b98f86ccec1f74365e4b748e16
ayazhanseitzhan <sae0708@mail.ru>
2022-04-24 16:38:34 +0800

    Add TODOs

--- prac02/exceptions_demo.py
+++ prac02/exceptions_demo.py
@@ -1,18 +1,12 @@
 """
 CP1404/CP5632 - Practical
-Answer the following questions:
-1. When will a ValueError occur?
-2. When will a ZeroDivisionError occur?
-3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Fill in the TODOs to complete the task
 """
 
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-print("Finished.")
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+    except:
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
