e2bf4ce9ec44b60ef8659f483838e014ff4263ed
ayazhanseitzhan <sae0708@mail.ru>
2022-04-24 16:27:29 +0800

    Add the introduction

---
+++ prac02/exceptions_demo.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
