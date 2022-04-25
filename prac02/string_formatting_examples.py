dac2a95b28e6489757fcb6b41527c095342dec63
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 15:50:38 +0800

    Add better version of string format

--- prac02/string_formatting_examples.py
+++ prac02/string_formatting_examples.py
@@ -4,3 +4,7 @@ cost = 16035.4
 
 # The 'old' manual way to format text with string concatenation:
 print("My guitar: " + name + ", first made in " + str(year))
+# A better way - using str.format():
+print("My guitar: {}, first made in {}".format(name, year))
+print("My guitar: {0}, first made in {1}".format(name, year))
+print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
