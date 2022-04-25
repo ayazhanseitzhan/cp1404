9b7dc6be9b8972a7dc8e37be9aa9a32d68ec2728
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 15:53:40 +0800

    Add correcting

--- prac02/string_formatting_examples.py
+++ prac02/string_formatting_examples.py
@@ -4,9 +4,11 @@ cost = 16035.4
 
 # The 'old' manual way to format text with string concatenation:
 print("My guitar: " + name + ", first made in " + str(year))
+
 # A better way - using str.format():
 print("My guitar: {}, first made in {}".format(name, year))
 print("My guitar: {0}, first made in {1}".format(name, year))
 print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
+
 # Formatting currency (grouping with comma, 2 decimal places):
 print("My {} would cost ${:,.2f}".format(name, cost))
