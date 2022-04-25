b4f1ef0ae2fc81a2ba4482b92cddeabcf730783b
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 15:48:22 +0800

    Add format text

---
+++ prac02/string_formatting_examples.py
@@ -0,0 +1,6 @@
+name = "Gibson L-5 CES"
+year = 1922
+cost = 16035.4
+
+# The 'old' manual way to format text with string concatenation:
+print("My guitar: " + name + ", first made in " + str(year))
