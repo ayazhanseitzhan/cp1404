76cad55a73e43f3e93c84cde3aa4123de97af4b9
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 15:08:21 +0800

    Add program to get and check user's password

--- prac02/password_checker.py
+++ prac02/password_checker.py
@@ -7,3 +7,21 @@ MIN_LENGTH = 2
 MAX_LENGTH = 6
 SPECIAL_CHARS_REQUIRED = False
 SPECIAL_CHARACTERS = "`~,./'[]<>?{}|\\!@#$%^&*()_-=+"
+
+
+def main():
+    """Program to get and check a user's password."""
+    print("Enter a valid password")
+    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
+          "characters, and contain:")
+    print("\t1 or more uppercase characters")
+    print("\t1 or more lowercase characters")
+    print("\t1 or more numbers")
+    if SPECIAL_CHARS_REQUIRED:
+        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+    print("Your {}-character password is valid: {}".format(len(password),
+                                                           password))
