893de29d4798388cfdab93944b5a290240605ede
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 16:02:45 +0800

    Add word genarator file

---
+++ prac02/word_generator.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 - Practical
+"""
+import random
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+word_format = "ccvcvvc"
+word = ""
+for kind in word_format:
+    if kind == "c":
+        word += random.choice(CONSONANTS)
+    else:
+        word += random.choice(VOWELS)
+
+print(word)
