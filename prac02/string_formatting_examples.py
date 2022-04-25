7072ece1d9da75b6434dd95d54c84b04d5ff408d
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 15:53:08 +0800

    Add formatting currency

--- prac02/string_formatting_examples.py
+++ prac02/string_formatting_examples.py
@@ -8,3 +8,5 @@ print("My guitar: " + name + ", first made in " + str(year))
 print("My guitar: {}, first made in {}".format(name, year))
 print("My guitar: {0}, first made in {1}".format(name, year))
 print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
+# Formatting currency (grouping with comma, 2 decimal places):
+print("My {} would cost ${:,.2f}".format(name, cost))
