5eb91ff700c17591d6a835a08af0dcc921940311
ayazhanseitzhan <sae0708@mail.ru>
2022-04-25 15:37:12 +0800

    Add exceptions file

--- prac02/exceptions_demo.py
+++ prac02/exceptions_demo.py
@@ -1,27 +1,18 @@
 """
 CP1404/CP5632 - Practical
-Password checker "skeleton" code to help you get started
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
-SPECIAL_CHARACTERS = "_-=+`~,./'[]<>?{}|\\!@#$%^&*()"
-
-
-def main():
-    """Program to get and check a user's password."""
-    print("Enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
-    password = input("> ")
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print("Your {}-character password is valid: {}".format(len(password),
-                                                           password))
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
