40ce8e06eba67e4a3806389bfa53ff71c79b9f00
ayazhanseitzhan <sae0708@mail.ru>
2022-05-10 17:51:38 +0800

    Add build function

--- prac07/squaring.py
+++ prac07/squaring.py
@@ -6,3 +6,11 @@ Started 10/05/2022
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+
+
+class SquareNumberApp(App):
+    def build(self):
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
