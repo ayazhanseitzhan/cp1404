f6a74f4066211f91a18001be77e333f0c52f2122
ayazhanseitzhan <sae0708@mail.ru>
2022-05-10 18:10:39 +0800

    Add class and two main functions

--- convert_miles_km.py
+++ convert_miles_km.py
@@ -2,3 +2,15 @@ from kivy.app import App
 from kivy.lang import Builder
 
 MILES_TO_KM = 1.60934
+
+class MilesConverter(App):
+
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+    def handle_calculate(self):
+        value = self.get_miles()
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
