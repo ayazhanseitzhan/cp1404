9857c6dbea9f948eaac5f09e5139d6132a8673f2
ayazhanseitzhan <sae0708@mail.ru>
2022-05-10 18:11:51 +0800

    Add two functions and GUI runner

--- convert_miles_km.py
+++ convert_miles_km.py
@@ -14,3 +14,17 @@ class MilesConverter(App):
         value = self.get_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, change):
+        value = self.get_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
+
+    def get_miles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+    MilesConverter().run()
