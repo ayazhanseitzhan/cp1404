006eb06ebfa75c5d4f4f183d9e7ba8bf73c25dfb
ayazhanseitzhan <sae0708@mail.ru>
2022-05-10 17:54:21 +0800

    Add build comment

--- prac07/squaring.py
+++ prac07/squaring.py
@@ -11,6 +11,7 @@ from kivy.core.window import Window
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
+        """ build the Kivy app from the kv file """
         Window.size = (200, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
