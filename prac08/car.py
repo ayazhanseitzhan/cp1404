8bff6b061c54bcb9fb2a7b737d1c6899ef952bc5
ayazhanseitzhan <sae0708@mail.ru>
2022-05-17 13:58:31 +0600

    Add amount to the car's fuel

--- prac08/car.py
+++ prac08/car.py
@@ -11,3 +11,27 @@ class Car:
 
         def __str__(self):
             return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
+
+
+def __str__(self):
+    return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
+
+
+def add_fuel(self, amount):
+    """Add amount to the car's fuel."""
+    self.fuel += amount
+
+
+def drive(self, distance):
+    """Drive the car a given distance.
+    Drive given distance if car has enough fuel
+    or drive until fuel runs out return the distance actually driven.
+    """
+    if distance > self.fuel:
+        distance = self.fuel
+        self.fuel = 0
+    else:
+        self.fuel -= distance
+    self.odometer += distance
+    return distance
+
